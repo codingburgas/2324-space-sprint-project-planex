6146a1644b576117b3ebfcdb83b549c190dca20e
Александър Мирославов Колев <AMKolev22@codingburgas.bg>
2024-02-08 15:43:09 +0200

    Add scalar product formula to core

---
+++ solar-physics/solar-physics/solar-physics.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "vector.h"
+
+int main() {
+	Vector::Vec3 vector(3,3.2,3.1);
+	vector.print();
+	
+}
--- solar-physics/solar-physics/vector.h
+++ solar-physics/solar-physics/vector.h
@@ -2,33 +2,33 @@
 #define real double
 
 #include <cmath>
+#include <iostream>
 
 namespace Vector {
 	class Vec3 {
 	public:
 		real x, y, z, magnitude;
 		Vec3(const real x, const real y, const real z) : x(x), y(y), z(z) {}; // constructor
-		Vec3() : x(0), y(0), z(0) {}; // default constructor
 		
-		void operator*=(const Vec3* vector) {
+		auto operator*=(const Vec3* vector) {
 			this->x *= vector->x;
 			this->y *= vector->y;
 			this->z *= vector->z;
 		}
 
-		void operator+=(const Vec3* vector) {
+		auto operator+=(Vec3* vector) {
 			this->x += vector->x;
 			this->y += vector->y;
 			this->z += vector->z;
 		}
 
-		void operator-=(const Vec3* vector) {
+		auto operator-=(const Vec3* vector) {
 			this->x -= vector->x;
 			this->y -= vector->y;
 			this->z -= vector->z;
 		}
 
-		Vec3 operator*(const real value) const {
+		Vec3 operator*(const real& value) const {
 			return Vec3(x * value, y * value, z * value);
 		}
 
@@ -46,22 +46,44 @@ namespace Vector {
 			vector->z = -(vector->z);
 		}
 
-		void getMagnitude() {
-			magnitude = sqrt(x * x + y * y + z * z);
+		auto getMagnitude3D(Vec3* vector) const {
+			return sqrt(vector->x * vector->x + vector->y * vector->y + vector->z * vector->z);
 		}
 
-		void toUnitVector(Vec3* vector) {
-			vector->getMagnitude();
-			real& mag = vector->magnitude;
-			if (mag > 0) {
-				vector->x = ((vector->x) / mag);
-				vector->y = ((vector->y) / mag);
-				vector->z = ((vector->z) / mag);
+		auto toUnitVector(Vec3* vector) const {
+			real magnitude = vector->getMagnitude3D(vector);
+			if (magnitude > 0) {
+				vector->x = ((vector->x) / magnitude);
+				vector->y = ((vector->y) / magnitude);
+				vector->z = ((vector->z) / magnitude);
+			}
+			else {
+				vector->x = 0, vector->y = 0, vector->z = 0;
 			}
 		}
 
+		void addScaledVector(const Vec3* vector, const real& scale) {
+			this->x = vector->x + vector->x * scale;
+			this->y = vector->y + vector->y * scale;
+			this->z = vector->z + vector->z * scale;
+		}
 
-	private:
-		real padding; // padding to optimize for 4 float nums;
+		void print() const {
+			std::cout << x << " " << y << " " << z;
+		}
+
+		Vec3 getChangePosVec3(Vec3* vector1, Vec3* vector2) {
+			return Vec3(fabs(vector1->x - vector2->x), fabs(vector1->y - vector2->x), fabs(vector1->z - vector2->z));
+		}
+
+		 auto scalarProduct(Vec3* vector) const {
+			 return (x * vector->x) + (y * vector->y) + (z * vector->z);
+		}
+
+	};
+
+	class Quartenion {
+	public:
+		real x, y, z, q;
 	};
 }
