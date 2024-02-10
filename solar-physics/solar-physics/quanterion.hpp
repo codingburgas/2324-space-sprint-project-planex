7fadbaeba4674dce876995ea4ea141a8e409cae3
AMKolev22 <amkolev22@codingburgas.bg>
2024-02-10 15:13:12 +0200

    Add acceleration. Close #10

---
+++ solar-physics/solar-physics/quanterion.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#define real double
+
+
+#include <cmath>
+#include <iostream>
+#include <vector>
+
+using std::cin, std::cout, std::endl;
+
+namespace Quanterion {
+	class Vec4 {
+	public:
+		real x, y, z, q;
+	};
+}
