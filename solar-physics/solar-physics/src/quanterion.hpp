690b1455c0c73d088187adf64ff0684fd51ed54d
Anton Teodorov Kabakov <ATKabakov22@codingburgas.bg>
2024-02-11 18:03:37 +0200

    Add getNormalizeQuaternion, isUnitQuaternion and getQuaternionMagnitude function

--- solar-physics/solar-physics/src/quanterion.hpp
+++ solar-physics/solar-physics/src/quanterion.hpp
@@ -55,5 +55,18 @@ namespace Quanterion {
         Vec4 operator/(const real& scalar) const {
             return Vec4(w / scalar, x / scalar, y / scalar, z / scalar);
         }
+
+        real getQuaternionMagnitude(Vec4& q) const {
+            return sqrt(q.w * q.w + q.x * q.x + q.y * q.y + q.z * q.z);
+        }
+
+        bool isUnitQuaternion(Vec4& q) const {
+            return fabs(getQuaternionMagnitude(q) - 1.0) < 1e-10;
+        }
+
+        Vec4 getNormalizeQuaternion(Vec4& q) const {
+            real magnitude = getQuaternionMagnitude(q);
+            return Vec4(q.w / magnitude, q.x / magnitude, q.y / magnitude, q.z / magnitude);
+        }
     };
 }
