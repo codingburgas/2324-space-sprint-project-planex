bd0a66cc2144ea2901ee61ad9a017c6f50d9c595
Anton Teodorov Kabakov <ATKabakov22@codingburgas.bg>
2024-02-16 23:17:26 +0200

    Add send and receive multiple messages in the server

--- Test/Test/Test.cpp
+++ Test/Test/Test.cpp
@@ -1,32 +1,51 @@
 #include <SFML/Network.hpp>
 #include <iostream>
 
+using std::cerr;
+using std::cout;
+
 int main() {
+    cout << "test\n\n";
     // Create a UDP socket
     sf::UdpSocket socket;
 
     // Bind the socket to a port
     unsigned short port = 12345;
     if (socket.bind(port) != sf::Socket::Done) {
-        std::cerr << "Failed to bind the socket to port " << port << std::endl;
+        cerr << "Failed to bind the socket to port " << port << std::endl;
         return 1;
     }
 
-    std::cout << "Server is listening on port " << port << std::endl;
+    cout << "Server is listening on port " << port << std::endl;
 
     // Receive data from clients
     char buffer[1024];
     std::size_t received;
+    std::string message;
     sf::IpAddress sender;
-    unsigned short senderPort;
+    unsigned short senderPort = 12346;
 
     while (true) {
-        if (socket.receive(buffer, sizeof(buffer), received, sender, senderPort) != sf::Socket::Done) {
-            std::cerr << "Failed to receive data from client" << std::endl;
+        // Receive data from clients
+        for (size_t i = 0; i < 5; i++)
+        {
+            if (socket.receive(buffer, sizeof(buffer), received, sender, senderPort) != sf::Socket::Done) {
+                cerr << "Failed to receive data from client" << std::endl;
+            }
+            else {
+                buffer[received] = '\0'; // Null-terminate the received data
+                cout << "Received: " << buffer << std::endl;
+            }
         }
-        else {
-            buffer[received] = '\0'; // Null-terminate the received data
-            std::cout << "Received: " << buffer << " from " << sender << ":" << senderPort << std::endl;
+        // Send data to the client
+        for (size_t i = 0; i < 5; i++)
+        {
+            cout << "Enter message to send to client: ";
+            std::getline(std::cin, message);
+
+            if (socket.send(message.c_str(), message.size() + 1, sender, senderPort) != sf::Socket::Done) {
+                cerr << "Failed to send data to client" << std::endl;
+            }
         }
     }
 
