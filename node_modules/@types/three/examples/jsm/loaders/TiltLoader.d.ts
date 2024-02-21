b9cbe97f398bd163fcc7f0e2e4ba3c9d45c5f479
amkolev22 <amkolev22@codingburgas.bg>
2024-02-21 17:44:04 +0200

    Upload cpp libraries

--- node_modules/@types/three/examples/jsm/loaders/TiltLoader.d.ts
+++
@@ -1,7 +0,0 @@
-import { Group, Loader, LoadingManager } from "../../../src/Three.js";
-
-export class TiltLoader extends Loader<Group> {
-    constructor(manager?: LoadingManager);
-
-    parse(data: ArrayBuffer): Group;
-}
