d004fbefd626648f1350a27ed8a27ec08b66f6e6
Anton Teodorov Kabakov <ATKabakov22@codingburgas.bg>
2024-02-25 00:59:27 +0200

    Add the path line of Uranus

--- app/renderer/src/solar-components/models/Uranus.tsx
+++ app/renderer/src/solar-components/models/Uranus.tsx
@@ -29,6 +29,25 @@ type GLTFResult = GLTF & {
   animations: GLTFAction[]
 }
 
+function CircleGeometry(radius: number, segments: number, center: THREE.Vector3): THREE.BufferGeometry {
+  const geometry = new THREE.BufferGeometry();
+  const vertices = [];
+
+
+  for (let i = 0; i <= segments; i++) {
+    const theta = (i / segments) * Math.PI * 2;
+    const x = center.x + radius * Math.cos(theta);
+    const y = center.y;
+    const z = center.z + radius * Math.sin(theta);
+    vertices.push(x, y, z);
+  }
+
+  geometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
+
+  return geometry;
+}
+
+
 type ActionName = 'rings|7. saturn.001Action.002' | 'rings|ringsAction' | 'rings|saturn_ringAction' | 'rings|Shader NodetreeAction' | 'rings|spatial cameraAction' | 'rings|SphereAction' | 'rings|SphereAction.002' | 'rings|uranus|Take 001|BaseLayer' | 'rings|venusAction' | 'rings|venusAction.001' | 'rings|venusAction.002' | 'rings|venusAction.003' | 'rings|venusAction.004' | 'rings|venusAction.005' | 'rings|venusAction.008' | 'uranus|7. saturn.001Action.002' | 'uranus|ringsAction' | 'uranus|saturn_ringAction' | 'uranus|spatial cameraAction' | 'uranus|SphereAction' | 'uranus|SphereAction.002' | 'uranus|uranus|Take 001|BaseLayer' | 'uranus|venusAction' | 'uranus|venusAction.001' | 'uranus|venusAction.002' | 'uranus|venusAction.003' | 'uranus|venusAction.004' | 'uranus|venusAction.005' | 'uranus|venusAction.008'
 interface GLTFAction extends THREE.AnimationClip {
   name: ActionName
@@ -40,26 +59,31 @@ export default function Model(props: JSX.IntrinsicElements['group']) {
   const { nodes, materials, animations } = useGLTF('../../../public/uranus.glb') as GLTFResult
   const { actions } = useAnimations(animations, group)
   return (
-    <group ref={group} {...props} dispose={null} position={[-330, 0, 0]}>
-      <group name="Sketchfab_Scene">
-        <group name="Sketchfab_model" rotation={[-Math.PI / 2, 0, 0]}>
-          <group name="f029815b00e641df84e4fa9461089ab2fbx" rotation={[Math.PI / 2, 0, 0]} scale={3}>
-            <group name="Object_2">
-              <group name="RootNode">
-                <group name="uranus" position={[0, 0, 0]} rotation={[-0.818, 1.177, 0.869]} scale={3}>
-                  <group name="rings">
-                    <mesh name="rings_lambert3_0" geometry={nodes.rings_lambert3_0.geometry} material={materials.lambert3} />
-                    <mesh name="rings_lambert4_0" geometry={nodes.rings_lambert4_0.geometry} material={materials.lambert4} />
-                    <mesh name="rings_lambert5_0" geometry={nodes.rings_lambert5_0.geometry} material={materials.lambert5} />
-                    <mesh name="rings_lambert2_0" geometry={nodes.rings_lambert2_0.geometry} material={materials.lambert2} />
+    <group>
+      <group ref={group} {...props} dispose={null} position={[-330, 0, 0]}>
+        <group name="Sketchfab_Scene">
+          <group name="Sketchfab_model" rotation={[-Math.PI / 2, 0, 0]}>
+            <group name="f029815b00e641df84e4fa9461089ab2fbx" rotation={[Math.PI / 2, 0, 0]} scale={3}>
+              <group name="Object_2">
+                <group name="RootNode">
+                  <group name="uranus" position={[0, 0, 0]} rotation={[-0.818, 1.177, 0.869]} scale={3}>
+                    <group name="rings">
+                      <mesh name="rings_lambert3_0" geometry={nodes.rings_lambert3_0.geometry} material={materials.lambert3} />
+                      <mesh name="rings_lambert4_0" geometry={nodes.rings_lambert4_0.geometry} material={materials.lambert4} />
+                      <mesh name="rings_lambert5_0" geometry={nodes.rings_lambert5_0.geometry} material={materials.lambert5} />
+                      <mesh name="rings_lambert2_0" geometry={nodes.rings_lambert2_0.geometry} material={materials.lambert2} />
+                    </group>
+                    <mesh name="uranus_uranus_shader_0" geometry={nodes.uranus_uranus_shader_0.geometry} material={materials.uranus_shader} />
                   </group>
-                  <mesh name="uranus_uranus_shader_0" geometry={nodes.uranus_uranus_shader_0.geometry} material={materials.uranus_shader} />
                 </group>
               </group>
             </group>
           </group>
         </group>
       </group>
+      <line geometry={CircleGeometry(450, 64, new THREE.Vector3(120, 0, 0))}>
+        <lineBasicMaterial color='blue' transparent opacity={2} />
+      </line>
     </group>
   )
 }
