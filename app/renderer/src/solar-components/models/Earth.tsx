b196863feb1831e85285d382fc2750d19df08e89
Anton Teodorov Kabakov <ATKabakov22@codingburgas.bg>
2024-02-25 00:50:08 +0200

    Fix the the list of the Earth

--- app/renderer/src/solar-components/models/Earth.tsx
+++ app/renderer/src/solar-components/models/Earth.tsx
@@ -27,11 +27,13 @@ function CircleGeometry(radius: number, segments: number, center: THREE.Vector3)
   const geometry = new THREE.BufferGeometry();
   const vertices = [];
 
+  
   for (let i = 0; i <= segments; i++) {
     const theta = (i / segments) * Math.PI * 2;
-    const x = center.x + radius * Math.cos(theta);
-    const y = center.y + radius * Math.sin(theta);
-    vertices.push(x, y, center.z);
+    const x = center.x + radius * Math.cos(theta); 
+    const y = center.y; 
+    const z = center.z + radius * Math.sin(theta); 
+    vertices.push(x, y, z);
   }
 
   geometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
@@ -40,25 +42,28 @@ function CircleGeometry(radius: number, segments: number, center: THREE.Vector3)
 }
 
 
+
 export default function Model(props: JSX.IntrinsicElements['group']) {
   const { nodes, materials } = useGLTF('../../../public/earth.glb') as GLTFResult;
 
   const scaleFactor = 10;
 
   return (
-    <group {...props} dispose={null} position={[-40, -5, 0]} scale={[scaleFactor, scaleFactor, scaleFactor]}>
-      <group rotation={[-Math.PI / 2, 0, 0]}>
-        {Object.keys(nodes).map((nodeName) => (
-          <mesh key={nodeName} geometry={nodes[nodeName].geometry} material={materials.heightmap_ref_group} />
-        ))}
-
-        {/* Add the circle */}
-        <line geometry={CircleGeometry(8.1, 64, new THREE.Vector3(8, 0, 0.5))}>
-          <lineBasicMaterial color='red' transparent opacity={2} />
-        </line>
-
+    <group>
+      <group {...props} dispose={null} position={[-40, -5, 0]} scale={[scaleFactor, scaleFactor, scaleFactor]}>
+        <group rotation={[-Math.PI / 2, 0, 0]}>
+          {Object.keys(nodes).map((nodeName) => (
+            <mesh key={nodeName} geometry={nodes[nodeName].geometry} material={materials.heightmap_ref_group} />
+          ))}
+        </group>
       </group>
+
+      <line geometry={CircleGeometry(160, 64, new THREE.Vector3(120, 0, 0))}>
+        <lineBasicMaterial color='blue' transparent opacity={2} />
+      </line>
     </group>
+
+   
   );
 }
 
