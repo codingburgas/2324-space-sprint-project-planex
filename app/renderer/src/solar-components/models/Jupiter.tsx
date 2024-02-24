30b9a73a4a4e27c49caf51da4eca4666a469488d
Anton Teodorov Kabakov <ATKabakov22@codingburgas.bg>
2024-02-25 01:03:46 +0200

    Add the path of Jupiter

--- app/renderer/src/solar-components/models/Jupiter.tsx
+++ app/renderer/src/solar-components/models/Jupiter.tsx
@@ -22,24 +22,48 @@ type GLTFResult = GLTF & {
   animations: GLTFAction[];
 };
 
+function CircleGeometry(radius: number, segments: number, center: THREE.Vector3): THREE.BufferGeometry {
+  const geometry = new THREE.BufferGeometry();
+  const vertices = [];
+
+
+  for (let i = 0; i <= segments; i++) {
+    const theta = (i / segments) * Math.PI * 2;
+    const x = center.x + radius * Math.cos(theta);
+    const y = center.y;
+    const z = center.z + radius * Math.sin(theta);
+    vertices.push(x, y, z);
+  }
+
+  geometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
+
+  return geometry;
+}
+
+
 export default function Model(props: JSX.IntrinsicElements['group']) {
   const { nodes, materials } = useGLTF('../../../public/jupiter.glb') as GLTFResult;
 
   const scaleFactor = 0.028;
 
   return (
-    <group {...props} dispose={null} position={[-160, 5, 0]} scale={[scaleFactor, scaleFactor, scaleFactor]}>
-      <group rotation={[-Math.PI / 2, 0, 0]}>
-        <group rotation={[Math.PI / 2, 0, 0]}>
-          {/* Apply scale to the mesh */}
-          <mesh
-            geometry={nodes.Sphere_Material_0.geometry}
-            material={materials.Material}
-            rotation={[-Math.PI / 2, 0, 0]}
-            scale={[scaleFactor * 101, scaleFactor * 101, scaleFactor * 101]}
-          />
+    <group>
+      <group {...props} dispose={null} position={[-160, 5, 0]} scale={[scaleFactor, scaleFactor, scaleFactor]}>
+        <group rotation={[-Math.PI / 2, 0, 0]}>
+          <group rotation={[Math.PI / 2, 0, 0]}>
+            {/* Apply scale to the mesh */}
+            <mesh
+              geometry={nodes.Sphere_Material_0.geometry}
+              material={materials.Material}
+              rotation={[-Math.PI / 2, 0, 0]}
+              scale={[scaleFactor * 101, scaleFactor * 101, scaleFactor * 101]}
+            />
+          </group>
         </group>
       </group>
+      <line geometry={CircleGeometry(280, 64, new THREE.Vector3(120, 0, 0))}>
+        <lineBasicMaterial color='blue' transparent opacity={2} />
+      </line>
     </group>
   );
 }
