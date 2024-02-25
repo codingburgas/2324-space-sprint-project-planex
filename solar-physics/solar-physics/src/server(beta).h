cc9c92596752d38f86a9cf83f965cb0bc9225f8d
amkolev22 <amkolev22@codingburgas.bg>
2024-02-25 18:57:02 +0200

    Fix cpp

---
+++ solar-physics/solar-physics/src/server(beta).h
@@ -0,0 +1,68 @@
+#pragma once
+#include <websocketpp/config/asio_no_tls.hpp>
+#include <websocketpp/server.hpp>
+#include <iostream>
+
+
+using websocketpp::lib::placeholders::_1;
+using websocketpp::lib::placeholders::_2;
+using websocketpp::lib::bind;
+
+#define SERVER_PORT 8000
+
+class Network {
+
+public:
+
+    websocketpp::server<websocketpp::config::asio> server;
+    uint16_t serverPort = SERVER_PORT;
+
+    void on_message(websocketpp::server<websocketpp::config::asio>* s, websocketpp::connection_hdl hdl, websocketpp::server<websocketpp::config::asio>::message_ptr msg) {
+        std::cout << msg->get_payload()
+            << std::endl;
+
+        if (msg->get_payload() == "stop-listening") {
+            s->stop_listening();
+            return;
+        }
+
+        try {
+            s->send(hdl, this->data, msg->get_opcode());
+        }
+        catch (websocketpp::exception const& e) {
+            std::cout << "Echo failed because: "
+                << "(" << e.what() << ")" << std::endl;
+        }
+    }
+
+    void setData(std::string& input) {
+        this->data = input;
+    }
+
+    void runServer() {
+        this->server.run();
+    }
+
+    void setServerPort(const uint16_t& port) {
+        this->serverPort = port;
+    }
+
+    void initServer() {
+        this->server.listen(this->serverPort);
+        this->server.start_accept();
+    }
+
+    void stopServer() {
+        this->server.stop_listening();
+    }
+
+    auto channelConfig() {
+        this->server.set_access_channels(websocketpp::log::alevel::all ^ websocketpp::log::alevel::frame_header);
+        this->server.clear_access_channels(websocketpp::log::alevel::frame_header);
+    }
+
+
+private:
+    std::string data;
+
+};
--- solar-physics/solar-physics/src/solar-physics.cpp
+++ solar-physics/solar-physics/src/solar-physics.cpp
@@ -1,17 +1,53 @@
+#define ASIO_STANDALONE
+#define _WEBSOCKETPP_CPP11_TYPE_TRAITS_
+
+
+#include <websocketpp/config/asio_no_tls.hpp>
+#include <websocketpp/server.hpp>
 #include <iostream>
-#include "vector.hpp"
-#include "quanterion.hpp"
-#include "utils.hpp"
 
 
-using Vector::Vec3;
-using Quanterion::Vec4;
-using std::cin, std::cout, std::endl;
+
+using websocketpp::lib::placeholders::_1;
+using websocketpp::lib::placeholders::_2;
+using websocketpp::lib::bind;
+
+#define SERVER_PORT 8000
+
+websocketpp::connection_hdl hdlReference;
+
+void on_message(websocketpp::server<websocketpp::config::asio>* s, websocketpp::connection_hdl hdl, websocketpp::server<websocketpp::config::asio>::message_ptr msg) {
+    std::cout << msg->get_payload()
+        << std::endl;
+    hdlReference = hdl;
+
+    if (msg->get_payload() == "stop-listening") {
+        s->stop_listening();
+        return;
+    }
+}
 
 int main() {
 
-    Utils::Memory obj(1024);
-    const buffer_var size = 24;
-    std::cout << obj.createHeapObject(obj, &size);
-    
+    websocketpp::server<websocketpp::config::asio> echo_server;
+
+        echo_server.set_access_channels(websocketpp::log::alevel::all ^ websocketpp::log::alevel::frame_header);
+        echo_server.clear_access_channels(websocketpp::log::alevel::frame_header);
+
+
+        echo_server.init_asio();
+
+
+        echo_server.set_message_handler(bind(&on_message, &echo_server, ::_1, ::_2));
+
+
+        echo_server.listen(SERVER_PORT);
+
+        echo_server.start_accept();
+
+        echo_server.run();
+   
+
+       
+
 }
