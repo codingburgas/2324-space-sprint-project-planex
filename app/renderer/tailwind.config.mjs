580f6009b0b550939e72094a018824f123d9990e
Александър Мирославов Колев <AMKolev22@codingburgas.bg>
2024-02-27 13:00:58 +0200

    Add aceternity ui

--- app/renderer/tailwind.config.mjs
+++ app/renderer/tailwind.config.mjs
@@ -1,8 +1,27 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+ 
+const colors = require("tailwindcss/colors");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
 	},
-	plugins: [],
-}
+	plugins: [addVariablesForColors],
+};
+
+function addVariablesForColors({ addBase, theme }) {
+	let allColors = flattenColorPalette(theme("colors"));
+	let newVars = Object.fromEntries(
+	  Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+	);
+   
+	addBase({
+	  ":root": newVars,
+	});
+  }
